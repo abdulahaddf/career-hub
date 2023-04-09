a68596aa18704eddcd203fbc2f999a9611ce3cd9
abdulahaddf <abdulahad.df1@gmail.com>
2023-04-09 22:56:24 +0600

    router fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-    
+    <Header></Header>
      
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App'
 import './index.css'
+import ErrorPage from './Components/ErrorPage'
+import Home from './Components/Home'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage/>,
+    // loader: productsAndCartData,
+    children: [
+      { path: '/', element: <Home></Home> },
+     
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+  <RouterProvider router={router}/>
+  </>
 )
+
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <>
+//     <RouterProvider router={router} />
+   
+//   </>
+// )
