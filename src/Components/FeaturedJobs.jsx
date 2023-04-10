0b2ebe6d0566538eb6e8672226f5efa8f82301cf
abdulahaddf <abdulahad.df1@gmail.com>
2023-04-10 23:14:43 +0600

    featured jobs showed

---
+++ src/Components/FeaturedJobs.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+const FeaturedJobs = ({ jobs }) => {
+  return (
+    <div className="w-fit mx-auto my-5 md:my-20">
+    <div className="text-center">
+    <h1 className="title ">Featured Jobs</h1>
+    <p>
+    Explore thousands of job opportunities with all the information you need. Its your future
+    </p>
+    </div>
+    <div className="grid md:grid-cols-2 gap-12 my-5 ">
+
+      {jobs.map((job) => (
+            //   const {logo, companyName, jobType, employmentType, location, salaryRange } = job;
+          <div className="card w-3/4 mx-auto glass p-5">
+          <figure>
+            <img
+              src={job.logo} alt="icon" className="w-64 md:w-9/12" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{job.jobTitle}</h2>
+            <h4>{job.companyName}</h4>
+            <div className="md:flex  text-center">
+                <div className="border mx-1 my-2 md:my-0 border-cyan-500 bg-cyan-500 rounded-md p-1 text-white">{job.jobType}</div>
+                <div className="border mx-1 border-cyan-500 bg-cyan-500 rounded-md p-1 text-white">{job.employmentType}</div>
+            </div>
+            <div className="md:flex md:justify-between">
+                <div >{job.location}</div>
+                <div >Salary:{job.salaryRange}</div>
+            </div>
+
+              <button className="btn btn-primary">View Details</button>
+
+
+           
+            
+          </div>
+        </div>
+      ))}
+    </div>
+    </div>
+  );
+};
+
+export default FeaturedJobs;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import Hero from './Hero';
 import Catagory from './Catagory';
+import FeaturedJobs from './FeaturedJobs';
 
 const Home = () => {
+    
+    const [jobs, setJobs] = useState([]);
+    console.log(jobs);
+  
+    useEffect(() => {
+      const url = "jobs.json";
+  
+      const fetchData = async () => {
+        try {
+          const response = await fetch(url);
+          const json = await response.json();
+          setJobs(json);
+        } catch (error) {
+          console.log("error", error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+
     return (
        <div>
         <Hero></Hero>
         <Catagory></Catagory>
+        <FeaturedJobs jobs={jobs}></FeaturedJobs>
        </div>
     );
 };
